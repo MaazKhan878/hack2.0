38d1d2ac62c600b8a30af1b92d47f64a17cdd50a
Maaz_Khan <maazkhan0348810@gmail.com>
2023-10-18 01:06:47 +0500

    User prompt

--- airDistance.c
+++ airDistance.c
@@ -4,6 +4,11 @@ int main(){
     double latitudeOrigin,latitudeDestination,longtitudeOrigin,longtitudeDestination;
     double airDistance, delta;
     const float rediousEarth= 6371;
+    printf("\t\tEnter the values in Degree\t\t\n");
+    printf("Enter the value of Latitude and Longtitude of Origin(Latitude,longtitude):");
+    scanf("%lf,%lf",&latitudeOrigin,&longtitudeOrigin);
+    printf("Enter the value of Latitude and Longtitude of Destination(latitude ,longtitude):");
+    scanf("%lf,%lf",&latitudeDestination,&longtitudeDestination);
 
     return 0;
 }
